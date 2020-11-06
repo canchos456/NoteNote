53b48f553454c102e49465431464ee4118f0d0cf
CREMA <crema@FAC1s-MacBookAir.local>
2020-11-06 16:47:11 +0900

    Changeroot based on if user_signed_in?

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
+  resources :notes
+
+  authenticate :user do
+  	root 'notes#index', as: "authenticate_root"
+  end
 
   root 'welcome#index'
 
-  resources :notes
+  
 end
 
